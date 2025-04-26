7eb34a94ed3cebc4d1a45df3dadbb2f9e6860060
AbyTob <bal.koksis.github@gmail.com>
2025-04-26 12:14:59 +0000

    auto update

--- cronjob.sh
+++ cronjob.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+# source the env variables, since cronjob does not load them from .bashrc directly 
+. $HOME/.bashrc
+
 echo ">> moving to folder"
+
 cd /root/Projects/abytob/python-daily-quiz
+
 echo "Executing make..."
+
 make setup_and_run
+
 echo "Done!"
