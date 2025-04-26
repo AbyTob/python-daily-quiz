5e5f5251b6b9ff521752376758631a33257b1ef5
AbyTob <bal.koksis.github@gmail.com>
2025-04-26 15:20:47 +0200

    cronjob log edit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # python-daily-quiz
+
+## Set-up crontab
+
+```bash
+chmod +x cronjob.sh
+```
+
+```cron
+# m h  dom mon dow   command
+* */8 * * * bash /<your_path>/cronjob.sh >> /<your_path>/cronlog.log 2>&1
+
+```
--- cronjob.sh
+++ cronjob.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# source the env variables, since cronjob does not load them from .bashrc directly 
+# source the env variables, since cronjob does not source .bashrc
 . $HOME/.bashrc
 
+echo ">> Current date" $(date)
 echo ">> moving to folder"
 
 cd ${$QUIZ_PROJECT_DIR} 
 
-echo "Executing make..."
+echo ">> Executing make..."
 
 make setup_and_run
 
-echo "Done!"
+echo ">> Done!"
